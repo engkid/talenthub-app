3e3f2dbded005be89d178dcaf56885024033c7bf
Engkit Satia Riswara <engkidsr@gmail.com>
2025-09-10 10:21:14 +0700

    change deeplink value, fallback url, ios and android url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <div className="min-h-screen w-full flex items-center justify-center">
       <DeepLinkButton
-        deepLink="myapp://home"
-        iosStoreUrl="https://apps.apple.com/app/id123456789"
-        androidStoreUrl="https://play.google.com/store/apps/details?id=com.example.app"
-        webFallbackUrl="https://example.com/download"
+        deepLink="erajaya://eraspace/product/apple-iphone-16"
+        iosStoreUrl="https://apps.apple.com/id/app/eraspace/id1534301787"
+        androidStoreUrl="https://play.google.com/store/apps/details?id=com.eraspace.app&hl=id"
+        webFallbackUrl="https://eraspace.com"
       >
         Open in App
       </DeepLinkButton>
