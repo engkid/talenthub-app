6a1d53de35b3a0b371db6c6abdd27a905e0b8dd6
Engkit Satia Riswara <engkidsr@gmail.com>
2025-10-02 20:13:46 +0700

    cover image when loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@
 
 import DeepLinkButton from "@/components/DeepLinkButton";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [isImageLoaded, setImageLoaded] = useState(false);
+
   return (
     <div className="flex min-h-dvh w-full items-center justify-center md:min-h-screen">
       <div className="hero-container relative overflow-hidden md:rounded-2xl md:shadow-xl">
@@ -13,7 +16,10 @@ export default function Home() {
           fill
           priority
           sizes="(max-width: 767px) 100vw, 640px"
-          className="object-cover md:object-contain"
+          className={`object-cover md:object-contain transition-opacity duration-75 ${
+            isImageLoaded ? "opacity-100" : "opacity-0"
+          }`}
+          onLoadingComplete={() => setImageLoaded(true)}
         />
         <div className="absolute inset-0 z-10 flex flex-col items-center justify-center gap-3 px-6 text-center">
           <DeepLinkButton
